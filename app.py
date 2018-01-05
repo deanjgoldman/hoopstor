cde324f579a30e246d67db9d4ad8112e0b79555e
Dean Goldman <deanjgoldman@gmail.com>
2018-01-05 11:55:48 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    return render_template('post' + str(post_id) + '.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/blogposts')
+def blogposts():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
