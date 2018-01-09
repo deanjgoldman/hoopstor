25b8856eae197ba0ea7fcf6228a9abca3d1bc579
Dean Goldman <deanjgoldman@gmail.com>
2018-01-09 13:42:15 -0500

    adding database

--- app.py
+++ app.py
@@ -1,12 +1,55 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from os import listdir
+import regex as re
 
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'static/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+
+db = SQLAlchemy(app)
+
+
+class Blogpost(db.Model):
+    __tablename__ = "blogpost"
+    userid = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    author = db.Column(db.String(20))
+    image = db.Column(db.Text)
+
+
+def get_htmls():
+    files = listdir(app.template_folder)
+    return files
+
+
+def get_context_data():
+    context = {}
+    texts = []
+    indices = []
+    files = get_htmls()
+    for filename in files:
+        if re.search('post', filename):
+            regex_num = re.search(r'\d+', filename)
+            filenumber = regex_num.group()
+            index = regex_num.span()[0]
+            html_text = filename[:index] + ' ' + filenumber
+            texts.append(html_text)
+            indices.append(filenumber)
+    context["post"] = dict(zip(texts, indices))
+    return context
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # context = None  # get_context_data()
+    context = Blogpost.query.all()
+    return render_template('index.html', context=context)
 
 
 @app.route('/post/<int:post_id>')
---
+++ database.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+from app import app, db, Blogpost
+import regex as re
+from os import listdir
+
+
+def main():
+    files = listdir(app.template_folder)
+    for html in files:
+        if re.search('post', html):
+            with open(app.template_folder + '/' + html, 'r') as html_doc:
+                soup = BeautifulSoup(html_doc.read(), 'html.parser')
+                title = soup.find('div', {"id": "title"}).h1.text
+                author = soup.find('span', {"id": "author"}).text
+                src = soup.find('img', {"id": "main"}).get('src')
+            post = Blogpost(title=title, author=author, image=src)
+            db.session.add(post)
+            db.session.commit()
