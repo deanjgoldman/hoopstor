1a3a5bf47de1caf7abd6f344cb69dc871536bda1
Dean Goldman <deanjgoldman@gmail.com>
2018-01-09 14:48:36 -0500

    database.py updater

--- database.py
+++ database.py
@@ -5,16 +5,17 @@ from os import listdir
 
 
 def main():
-    db.drop_all()
-    db.create_all()
+    ids = [str(post.userid) for post in app.Blogpost.query.all()]
     files = listdir(app.template_folder)
     for html in files:
         if re.search('post', html):
-            with open(app.template_folder + '/' + html, 'r') as html_doc:
-                soup = BeautifulSoup(html_doc.read(), 'html.parser')
-                title = soup.find('div', {"id": "title"}).h1.text
-                author = soup.find('span', {"id": "author"}).text
-                src = soup.find('img', {"id": "main"}).get('src')
-            post = Blogpost(title=title, author=author, image=src)
-            db.session.add(post)
-            db.session.commit()
+            num = re.search(r'\d+', html)
+            if not num.group() in ids:
+                with open(app.template_folder + '/' + html, 'r') as html_doc:
+                    soup = BeautifulSoup(html_doc.read(), 'html.parser')
+                    title = soup.find('div', {"id": "title"}).h1.text
+                    author = soup.find('span', {"id": "author"}).text
+                    src = soup.find('img', {"id": "main"}).get('src')
+                post = Blogpost(title=title, author=author, image=src)
+                db.session.add(post)
+                db.session.commit()
