b13e3a78036980b51016115b71c5526d62cf104a
Dean Goldman <deanjgoldman@gmail.com>
2018-01-09 13:47:08 -0500

    moving imports, edit requirements

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from os import listdir
-import regex as re
-
+import os
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'static/'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 db = SQLAlchemy(app)
@@ -23,31 +20,8 @@ class Blogpost(db.Model):
     image = db.Column(db.Text)
 
 
-def get_htmls():
-    files = listdir(app.template_folder)
-    return files
-
-
-def get_context_data():
-    context = {}
-    texts = []
-    indices = []
-    files = get_htmls()
-    for filename in files:
-        if re.search('post', filename):
-            regex_num = re.search(r'\d+', filename)
-            filenumber = regex_num.group()
-            index = regex_num.span()[0]
-            html_text = filename[:index] + ' ' + filenumber
-            texts.append(html_text)
-            indices.append(filenumber)
-    context["post"] = dict(zip(texts, indices))
-    return context
-
-
 @app.route('/')
 def index():
-    # context = None  # get_context_data()
     context = Blogpost.query.all()
     return render_template('index.html', context=context)
 
